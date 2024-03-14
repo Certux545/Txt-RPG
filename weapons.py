39733bc1fd634f27df757084193734a9bdb3238a
Certux545 <115732919+Certux545@users.noreply.github.com>
2024-03-14 18:38:31 +0100

    weapons.py
    script where you can add and edit weapons

--- weapons.py
+++ weapons.py
@@ -1,4 +1,7 @@
+class Weapon:
+    def __init__(self, name, damage):
+        self.name = name
+        self.damage = damage
 
-class sword:
-    damage = 2
-       
+sword = Weapon("Schwert", 5)
+fist = Weapon("Faust", 2)
